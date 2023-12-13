4150d282f7ff773cef2424503d6972c3fd451715
VALENTIN DIAZ <valentin.diaz@uc.cl>
2023-12-13 13:55:06 -0300

    simple connection and select all tasks

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 require('dotenv').config();
 
+const {Pool} = require('pg');
+
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const DATABASE_PORT = process.env.DATABASE_PORT;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_HOST= process.env.DATABASE_HOST
+
+const pool = new Pool({
+    user: DATABASE_USER,
+    host: DATABASE_HOST,
+    database: DATABASE_NAME,
+    password: DATABASE_PASSWORD,
+    port: DATABASE_PORT
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+app.get('/', async (req, res) => {
+    const queryString = 'select * from tasks;';
+    const { rows } = await pool.query(queryString)
+    res.json(rows)
 })
 
 const PORT = process.env.PORT || 5000;
