db93f850784b178e677f5a870574420afb78dbd2
VALENTIN DIAZ <valentin.diaz@uc.cl>
2023-12-20 18:16:30 -0300

    feat: order tasks by date

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get('/tasks', async (req, res) => {
-    const queryString = 'select * from tasks;';
+    const queryString = 'select * from tasks order by createdAt desc;';
     const { rows } = await pool.query(queryString)
     res.json(rows)
 });
