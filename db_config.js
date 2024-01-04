e52cc5b0bf1b86ff9cb2f226e27b8f90144632e7
VALENTIN DIAZ <valentin.diaz@uc.cl>
2024-01-04 11:03:40 -0300

    fix: add missing env variable

--- db_config.js
+++ db_config.js
@@ -5,6 +5,7 @@ const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_PORT = process.env.DATABASE_PORT;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 const DATABASE_HOST= process.env.DATABASE_HOST;
+const DATABASE_LINK = process.env.DATABASE_LINK;
 
 let pool;
 if (process.env.NODE_ENV === 'production') {
