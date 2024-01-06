8e96550464c62de7aec8889deb2c0f922adaa5f0
VALENTIN DIAZ <valentin.diaz@uc.cl>
2024-01-06 19:09:07 -0300

    feat: send data for first two plots

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,10 +4,31 @@ const router = express.Router();
 const { pool } = require('../db_config');
 
 router.get('/', async (req, res) => {
-    const queryString = 'select category, sum(reported_hours) from tasks where is_complete = true group by category';
-    const { rows } = await pool.query(queryString);
-    res.json({
-        timePerCategory: rows,
+    let queryString;
+    let rows;
+    
+    // Gráfico 1: Tiempo por cada categoría
+    queryString = 'select category, sum(reported_hours) from tasks where is_complete = true group by category;';
+    ({ rows: timePerCategory } = await pool.query(queryString));
+
+    // Gráfico 2: Eficiencia de tareas
+    // Tareas completadas
+    queryString = 'select count(*) from tasks where is_complete = true;';
+    ({ rows } = await pool.query(queryString));
+    const completedTasks = parseInt(rows[0].count);
+    
+    // Total de tareas
+    queryString = 'select count (*) from tasks;';
+    ({ rows } = await pool.query(queryString));
+    const totalTasks = parseInt(rows[0].count);
+    
+    
+    res.json({  
+        timePerCategoryPlot: timePerCategory,
+        taskCompletion: {
+            completedTasks,
+            totalTasks
+        }
     })
 })
 
