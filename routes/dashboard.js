0c51db8a2c6d4787ff149b8b14a45a057b51e77d
VALENTIN DIAZ <valentin.diaz@uc.cl>
2024-01-07 19:27:19 -0300

    feat: send data for all plots

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -21,13 +21,28 @@ router.get('/', async (req, res) => {
     queryString = 'select count (*) from tasks;';
     ({ rows } = await pool.query(queryString));
     const totalTasks = parseInt(rows[0].count);
+
+    // Gráfico 3: Categoría con más tareas creadas
+    queryString = "select category, count(*) from tasks group by category order by count(*) desc;";
+    const { rows: tasksPerCategory } = await pool.query(queryString);
+
+    // Gráfico 4: Porcentaje de días dedicados a Literature en el último mes
+    const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+    queryString = 'select createdAt::date, count(*) from tasks where category = \'Música\' and createdAt::date > $1::date - 30  group by createdAt::date order by createdAt::date desc ;';
+    const { rows: tasksPerDay } = await pool.query(queryString, [date])
+    const daysWithTaskCreated = tasksPerDay.length
     
     
     res.json({  
         timePerCategoryPlot: timePerCategory,
-        taskCompletion: {
+        taskCompletionPlot: {
             completedTasks,
             totalTasks
+        },
+        tasksPerCategoryPlot: tasksPerCategory,
+        daysWithTaskCreatedPlot: {
+            dayRange: 30,
+            daysWithTaskCreated
         }
     })
 })
